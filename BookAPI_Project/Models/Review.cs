b3d9322e5c74d7ac3b15bcc4bc8abb8e6fdf4559
Andre Lussier <Dremon7@hotmail.com>
2021-02-26 18:42:23 -0500

    Add project files.

---
+++ BookAPI_Project/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAPI_Project.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Headline { get; set; }
+        public string ReviewTest { get; set; }
+
+        public int Rating { get; set; }
+
+    }
+}
